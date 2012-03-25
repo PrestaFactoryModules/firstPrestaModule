30de147dadee3608c65f8ff9439483e2045352cc
kzone <PrestaFactoryModules@gmail.com>
2012-03-25 22:58:09 +0200

    delete init code

--- firstPrestaModule.php
+++ firstPrestaModule.php
@@ -2,30 +2,6 @@
 
 
 
-
-/**
- * NE DEVRAIT PAS ETRE DANS CE FICHIER !
-  * les déclarations ci-dessous ne devraient pas se trouver ici ...
-* Quelques petits problème d'__autoload à corriger pour palier à ce contretemps ! :)
-*/
-
-define('CLASS_DIR','/var/www/prestashop/classes');
-define ('CONFIG_DIR','/var/www/prestashop/config');
-define('PHPUNIT_DIR','/usr/share/php/PHPUnit34/usr/share/php/PHPUnit');//version 3.4
-define('TEST_DIR','/var/www/prestashop/modules/firstPrestaModule');
-		
-		
-set_include_path (get_include_path().PATH_SEPARATOR . CLASS_DIR);
-set_include_path (get_include_path().PATH_SEPARATOR . CONFIG_DIR);
-set_include_path (get_include_path().PATH_SEPARATOR . PHPUNIT_DIR);
-set_include_path (get_include_path().PATH_SEPARATOR . TEST_DIR);
-/**
-* Any other component that registers an autoloader unregisters the default __autoload().
-*
-*/
-require_once 'autoload.php';
-spl_autoload_register('__autoload');
-
 /**
 * ======================= DEPLACER DANS LE BOOTSTRAP DES TESTS =========================
 * ============     Ci dessous le code du module firstPrestaModule       ================
