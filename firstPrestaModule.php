b752967cb7be7ce568e7ce4a8881f81987375ea3
kzone <PrestaFactoryModules@gmail.com>
2012-03-25 23:02:10 +0200

    another

--- firstPrestaModule.php
+++ firstPrestaModule.php
@@ -1,19 +1,5 @@
 <?php
 
-
-
-/**
-* ======================= DEPLACER DANS LE BOOTSTRAP DES TESTS =========================
-* ============     Ci dessous le code du module firstPrestaModule       ================
-* ============     qui est implémenté petit à petit à l'aide de tests   ================
-* ============     utilisant les librairies PHPUnit (v3.4) !!!          ================
-*
-* ================= Ce fichier ne devrait comporter que ce qui suit ====================
-*/
-
-
-
-
 /* ======= Module firstPrestaModule ===== */
 class firstPrestaModule extends Module {
 	
